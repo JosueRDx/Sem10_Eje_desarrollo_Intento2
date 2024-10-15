cc3ba8a7b9bdd289c03de1c13436466b524c9f84
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-10-14 23:19:15 -0500

    feat: integrate ProductoApp into MainActivity

--- app/src/main/java/com/josuerdx/sem10_e_desarrollo_intento2/MainActivity.kt
+++ app/src/main/java/com/josuerdx/sem10_e_desarrollo_intento2/MainActivity.kt
@@ -3,25 +3,16 @@ package com.josuerdx.sem10_e_desarrollo_intento2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.josuerdx.sem10_e_desarrollo_intento2.ui.theme.Sem10_E_desarrollo_intento2Theme
+import com.josuerdx.sem10_e_desarrollo_intento2.view.ProductoApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Sem10_E_desarrollo_intento2Theme {
-                
+                ProductoApp()
             }
         }
     }
 }
-
